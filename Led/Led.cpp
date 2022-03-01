08d290e8943ffaa212894449646a8f5f16e51002
semidongdong <semidongdong@gmail.com>
2022-03-01 13:58:09 +0900

    first commit

---
+++ Led/Led.cpp
@@ -0,0 +1,43 @@
+#include "Led.h"
+
+// Led class의 생성자이다. :: scope(뭐의 뭐인지)
+Led::Led(int pin) : pin(pin) // 멤버변수(지역매개변수)
+{
+  // 지역변수 -> 멤버변수 -> 전역변수
+
+  pinMode(pin, OUTPUT);
+}
+
+Led::~Led()
+{
+}
+
+void Led::on()
+{
+  digitalWrite(pin, HIGH);
+}
+
+void Led::off()
+{
+  digitalWrite(pin, LOW);
+}
+
+void Led::toggle()
+{
+  int v = !digitalRead(pin); // 현재 출력 값 읽기
+  digitalWrite(pin, v);
+}
+
+void Led::blink(int delay_time)
+{
+  toggle();
+  if (delay_time > 0)
+  {
+    delay(delay_time);
+  }
+}
+
+void Led::setValue(int value)
+{
+  digitalWrite(pin, value);
+}
---
+++ Led/Led.h
@@ -0,0 +1,27 @@
+#ifndef LED_H
+#define LED_H
+
+#pragma once
+
+#include <Arduino.h>
+
+class Led
+{
+protected:
+    // 멤버 변수
+    int pin;
+
+public:
+    Led(int pin); // 생성자
+    ~Led();       // 소멸자
+
+    void on();
+    void off();
+    void toggle();
+    void blink(int delay_time = 0);
+    void setValue(int value);
+
+private:
+};
+
+#endif
